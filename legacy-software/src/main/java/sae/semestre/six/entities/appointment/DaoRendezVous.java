a4f3c4123ed823875b9c140808680dc459043761
THOMAS LEMAIRE <thomas.lemaire@iut.local>
2025-04-01 16:55:34 +0200

    Suppression DaoHibernateAbstrait.java & DaoGenerique.java

--- legacy-software/src/main/java/sae/semestre/six/entities/appointment/DaoRendezVous.java
+++ legacy-software/src/main/java/sae/semestre/six/entities/appointment/DaoRendezVous.java
@@ -1,11 +1,11 @@
 package sae.semestre.six.entities.appointment;
 
-import sae.semestre.six.base.DaoGenerique;
-import sae.semestre.six.entities.appointment.Appointment;
+import sae.semestre.six.base.GenericDao;
+
 import java.util.Date;
 import java.util.List;
 
-public interface DaoRendezVous extends DaoGenerique<Appointment, Long> {
+public interface DaoRendezVous extends GenericDao<Appointment, Long> {
     List<Appointment> trouverParIdPatient(Long idPatient);
     List<Appointment> trouverParIdMedecin(Long idMedecin);
     List<Appointment> trouverParPlageDates(Date dateDebut, Date dateFin);
--- legacy-software/src/main/java/sae/semestre/six/entities/appointment/ImplDaoRendezVous.java
+++ legacy-software/src/main/java/sae/semestre/six/entities/appointment/ImplDaoRendezVous.java
@@ -1,17 +1,18 @@
 package sae.semestre.six.entities.appointment;
 
-import sae.semestre.six.base.DaoHibernateAbstrait;
 import org.springframework.stereotype.Repository;
+import sae.semestre.six.base.AbstractHibernateDao;
+
 import java.util.Date;
 import java.util.List;
 
 @Repository
-public class ImplDaoRendezVous extends DaoHibernateAbstrait<Appointment, Long> implements DaoRendezVous {
+public class ImplDaoRendezVous extends AbstractHibernateDao<Appointment, Long> implements DaoRendezVous {
     
     @Override
     @SuppressWarnings("unchecked")
     public List<Appointment> trouverParIdPatient(Long idPatient) {
-        return getGestionnaireEntite()
+        return getEntityManager()
                 .createQuery("FROM Appointment WHERE patient.id = :idPatient")
                 .setParameter("idPatient", idPatient)
                 .getResultList();
@@ -20,7 +21,7 @@ public class ImplDaoRendezVous extends DaoHibernateAbstrait<Appointment, Long> i
     @Override
     @SuppressWarnings("unchecked")
     public List<Appointment> trouverParIdMedecin(Long idMedecin) {
-        return getGestionnaireEntite()
+        return getEntityManager()
                 .createQuery("FROM Appointment WHERE medecin.id = :idMedecin")
                 .setParameter("idMedecin", idMedecin)
                 .getResultList();
@@ -29,7 +30,7 @@ public class ImplDaoRendezVous extends DaoHibernateAbstrait<Appointment, Long> i
     @Override
     @SuppressWarnings("unchecked")
     public List<Appointment> trouverParPlageDates(Date dateDebut, Date dateFin) {
-        return getGestionnaireEntite()
+        return getEntityManager()
                 .createQuery("FROM Appointment WHERE dateRendezVous BETWEEN :dateDebut AND :dateFin")
                 .setParameter("dateDebut", dateDebut)
                 .setParameter("dateFin", dateFin)
